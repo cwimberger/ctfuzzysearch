0fc25eddf04154a90a8f5277fdfa87c5bad36e7e
Christoph Wimberger <christoph@wimberger.org>
2014-07-02 12:35:47 +0200

    Remove unnecessary project code.

--- FuzzySearch/CTAppDelegate.h
+++
@@ -1,9 +0,0 @@
-//  Copyright (c) 2014 Christoph Wimberger. All rights reserved.
-
-#import <UIKit/UIKit.h>
-
-@interface CTAppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
-
-@end
